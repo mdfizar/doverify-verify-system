746cdd658c9390f75a8f91f715952c3a66d5ae5d
mdfizar <mdfizarrana@gmail.com>
2024-08-18 13:59:19 +0600

    update

--- product-verify-system/assets/js/main.js
+++ product-verify-system/assets/js/main.js
@@ -49,10 +49,12 @@ function productLocaiton() {
   downThree.style.display ="block";
 }
 // hide show btn 
-const hideShowItem = document.querySelector(".product-details");
-const hideShowItemIcon = document.querySelector(".show-hide-btn i");
+// genuin product===
+const genuinSction = document.querySelector(".geneuin-product-container");
+const hideShowItem = genuinSction.querySelector(".product-details");
+const hideShowItemIcon = genuinSction.querySelector(".show-hide-btn i");
 
-function hideShowButton() {
+function geneuinProductHideShowButton() {
  if(hideShowItem.style.transform ==="translateY(0px)") {
   hideShowItem.style.transform ="translateY(265px)";
   hideShowItemIcon.style.transform ="rotate(180deg)";
@@ -61,5 +63,36 @@ function hideShowButton() {
   hideShowItem.style.transform ="translateY(0px)"
   hideShowItemIcon.style.transform ="rotate(0deg)";
  }
-  
-}
+};
+// product sold 
+const productSoldSction = document.querySelector(".product-sold-container");
+const soldHideShowItem = productSoldSction.querySelector(".product-details");
+const soldHideShowItemIcon = productSoldSction.querySelector(".show-hide-btn i");
+const soldLocationIcon = productSoldSction.querySelector(".arrow3");
+soldLocationIcon.style.display ="block"
+soldLocationIcon.style.color ="red"
+function productSoldHideShowButton() {
+  if(soldHideShowItem.style.transform ==="translateY(0px)") {
+   soldHideShowItem.style.transform ="translateY(265px)";
+   soldHideShowItemIcon.style.transform ="rotate(180deg)";
+  }
+  else{
+   soldHideShowItem.style.transform ="translateY(0px)"
+   soldHideShowItemIcon.style.transform ="rotate(0deg)";
+  }
+ };
+// no product 
+const noProductSction = document.querySelector(".noproduct-container");
+const noProductHideShowItem = noProductSction.querySelector(".product-details");
+const noProductshowItemIcon = noProductSction.querySelector(".show-hide-btn i");
+function noProductSoldHideShowButton() {
+  if(noProductHideShowItem.style.transform ==="translateY(0px)") {
+    noProductHideShowItem.style.transform ="translateY(265px)";
+    noProductshowItemIcon.style.transform ="rotate(180deg)";
+  }
+  else{
+    noProductHideShowItem.style.transform ="translateY(0px)"
+    noProductshowItemIcon.style.transform ="rotate(0deg)";
+  }
+ };
+
